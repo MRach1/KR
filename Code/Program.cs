f3c976a1415cb64b36928dd1c9c1185db153d61c
Rach <rach_manukyan@mail.ru>
2023-01-26 19:50:12 +0300

    код готов

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"word", "333", ":-)", "Moskov", "red", "homework", "21"};
+string[] Three(string[] arr)
+{
+    string[] result = new string[arr.Length];
+    int j = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length < 4)
+        {
+            result[j] = arr[i];
+            j ++;
+        }
+    }
+    return result;
+}
+void PrintArray(string[] ar)
+{
+    for (int i = 0; i < ar.Length; i++)
+    {
+        Console.Write($"{ar[i]} ");
+    }
+}
+PrintArray(Three(array));
